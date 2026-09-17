db8c5b6bc4873aeda4362a066289be10f196fda6
omaralsabbagh86 <omar.alsabbagh86@gmail.com>
2026-09-17 11:11:55 +0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,22 +1,13 @@
-const CACHE = 'pallet-tally-v2';
-const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
-});
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ).then(() => self.clients.claim()));
-});
-self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
-  e.respondWith(
-    caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
-      if (res && res.status === 200 && res.type === 'basic') {
-        const copy = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, copy));
-      }
-      return res;
-    }).catch(() => caches.match('./index.html')))
-  );
+/* Carton Link V3 service worker: network-first for the app page (picks up new versions), cache fallback offline */
+var CACHE = "carton-link-v3";
+var FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+self.addEventListener("install", function(e){ e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(FILES); }).then(function(){ return self.skipWaiting(); })); });
+self.addEventListener("activate", function(e){ e.waitUntil(caches.keys().then(function(ks){ return Promise.all(ks.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); })); }).then(function(){ return self.clients.claim(); })); });
+self.addEventListener("fetch", function(e){
+  if(e.request.method !== "GET") return;
+  var url = new URL(e.request.url);
+  if(url.origin !== location.origin) return;
+  e.respondWith(fetch(e.request).then(function(res){
+    var copy = res.clone(); caches.open(CACHE).then(function(c){ c.put(e.request, copy); }); return res;
+  }).catch(function(){ return caches.match(e.request).then(function(m){ return m || caches.match("./index.html"); }); }));
 });
